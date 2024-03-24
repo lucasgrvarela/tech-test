20a3f9845f2759e7194f17684fe87d8a7b8eeb3f
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2024-03-24 15:44:18 -0300

    update

--- metallb/fix-ip.sh
+++ metallb/fix-ip.sh
@@ -1,5 +1,5 @@
 # Extract IPv4 range from Docker network inspect output
-BASE_IP=$(docker network inspect kind | jq -r '.[0].IPAM.Config[0].Gateway' | cut -d "/" -f 1)
+BASE_IP=$(docker network inspect kind | jq -r '.[0].Containers | values[] | .IPv4Address' | cut -d "/" -f 1 | head -n1)
 IP_START=$(echo $BASE_IP | awk -F '[./]' '{print $1 "." $2 "." 255 "." 200}')
 IP_END=$(echo $BASE_IP | awk -F '[./]' '{print $1 "." $2 "." 255 "." 250}')
 
